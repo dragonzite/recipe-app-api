560c7093585265a55be23afa38752c2c9317ebce
dragonzite <dragonzite@gmail.com>
2020-07-21 23:25:46 -0700

    Added custom user model

---
+++ app/core/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user
+
+        Args:
+            email (str): users email address for account login/communication
+            password (str, optional): base64 encoded password.
+            Defaults to None.
+
+        Returns:
+            user object
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser
+
+        Args:
+            email (str): staff members email
+            password (str): base64 encoded password
+        """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username
+
+    Args:
+        AbstractBaseUser (obj): from django.contrib.auth.models
+        PermissionsMixin (obj): from django.contriv.auth.models
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
