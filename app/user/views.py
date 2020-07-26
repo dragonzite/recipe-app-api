93a6677326d355039e9509f5fa5d2ff09053646b
dragonzite <dragonzite@gmail.com>
2020-07-26 09:18:59 -0700

    added end point for creating and auth users

---
+++ app/user/views.py
@@ -0,0 +1,25 @@
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import UserSerializer, AuthTokenSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system
+
+    Args:
+        generics (obj): rest_framework module
+    """
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user
+
+    Args:
+        ObtainAuthToken (obj): from rest_framework
+    """
+
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
