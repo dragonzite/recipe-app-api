3725f507fa8d33496f6f952ea188dca37464ad3c
dragonzite <dragonzite@gmail.com>
2020-08-01 13:27:39 -0700

    Added Ingrient stuff

--- app/core/models.py
+++ app/core/models.py
@@ -67,3 +67,20 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredient to be used in recipe
+
+    Args:
+        models (obj): django model object
+    """
+
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
